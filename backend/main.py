37ac196e32eab547cfd6bb18284ad1594de0c426
ThakerJayam <jayam.thaker@gmail.com>
2021-11-25 18:59:22 -0800

    with working model

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,23 @@
+from fastai.basic_train import load_learner
+from fastai.text import learner
 from flask import Flask,current_app,request,jsonify
 from flask.globals import request
 from flask.wrappers import Response
 from flask_pymongo import PyMongo
-
+from dotenv import dotenv_values
+from fastai.vision import *
 app = Flask(__name__)
 
 app.app_context().push()
-app.config.from_envvar('env_var')
+
+config = dotenv_values(".env")
+app.config['MONGO_URI'] = config["MONGO_URI"]
+
+path = Path("")
+
+learner = load_learner(path,"model")
 db = PyMongo(current_app).db
+
 
 @app.route("/")
 def hello_world():
@@ -23,3 +33,26 @@ def signUp():
     
     db.users.insert_one({"username":username,"password":password})
     return "done"
+
+@app.route("/checkImage",methods = ['POST'])
+def checkImage():
+    img = request.files.get("image")
+    #print(img)
+    return classify(img)
+
+def classify(img_path):
+    
+    img = open_image(img_path)
+    pred = str(learner.predict(img)[0])
+
+    if(pred == "0"):
+        return "Negligible Possibility"
+    if(pred == "1"):
+        return "Mild Possibility"
+    if(pred == "2"):
+        return "Moderate Possibility"
+    if(pred == "3"):
+        return "Severe Possibility"
+
+
+    return "Please Try again later"
